d796c4a104dde66569a3b472ff468259ef287c2c
armistcxy <adlehoang118@gmail.com>
2024-04-21 18:59:27 +0700

    Add: done 212, 234: hybrid image

--- HW2/ex212.py
+++ HW2/ex212.py
@@ -6,22 +6,19 @@ import math
 
 
 def DFT_slow(data):
-  """
-  Implement the discrete Fourier Transform for a 1D signal
-  params:
-    data: Nx1: (N, ): 1D numpy array
-  returns:
-    DFT: Nx1: 1D numpy array 
-  """
-  N = len(data)
-  DFT = np.zeros((N,), complex)
-  for k in range(N):
-    for n in range(N):
-      DFT[k] += data[n] * np.exp(-2j * np.pi * k * n / N)
-  return DFT
-
-  
- 
+    """
+    Implement the discrete Fourier Transform for a 1D signal
+    params:
+      data: Nx1: (N, ): 1D numpy array
+    returns:
+      DFT: Nx1: 1D numpy array
+    """
+    N = len(data)
+    DFT = np.zeros((N,), complex)
+    for s in range(N):
+        for n in range(N):
+            DFT[s] += data[n] * np.exp(-2j * np.pi * s * n / N)
+    return DFT
 
 
 def show_img(origin, row_fft, row_col_fft):
@@ -31,18 +28,18 @@ def show_img(origin, row_fft, row_col_fft):
     params:
         origin: (H, W): 2D numpy array
         row_fft: (H, W): 2D numpy array
-        row_col_fft: (H, W): 2D numpy array    
+        row_col_fft: (H, W): 2D numpy array
     """
     fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(12, 8))
-    axs[0].imshow(origin, cmap='gray')
-    axs[0].set_title('Original Image')
-    axs[0].axis('off')
-    axs[1].imshow(np.log(np.abs(np.fft.fftshift(row_fft))), cmap='gray')
-    axs[1].set_title('Row-wise FFT')
-    axs[1].axis('off')
-    axs[2].imshow((np.log(np.abs(np.fft.fftshift(row_col_fft)))), cmap='gray')
-    axs[2].set_title('Column-wise FFT')
-    axs[2].axis('off')
+    axs[0].imshow(origin, cmap="gray")
+    axs[0].set_title("Original Image")
+    axs[0].axis("off")
+    axs[1].imshow(np.log(np.abs(np.fft.fftshift(row_fft))), cmap="gray")
+    axs[1].set_title("Row-wise FFT")
+    axs[1].axis("off")
+    axs[2].imshow((np.log(np.abs(np.fft.fftshift(row_col_fft)))), cmap="gray")
+    axs[2].set_title("Column-wise FFT")
+    axs[2].axis("off")
     plt.show()
 
 
@@ -52,29 +49,27 @@ def DFT_2D(gray_img):
     Note that: dtype of the output should be complex_
     params:
         gray_img: (H, W): 2D numpy array
-        
+
     returns:
         row_fft: (H, W): 2D numpy array that contains the row-wise FFT of the input image
         row_col_fft: (H, W): 2D numpy array that contains the column-wise FFT of the input image
     """
     # You need to implement the DFT here
+    H, W = gray_img.shape
+    row_fft = np.fft.fft(gray_img, axis=1)
 
-  
+    row_col_fft = np.fft.fft(row_fft, axis=0)
+
+    return row_fft, row_col_fft
 
 
+if __name__ == "__main__":
 
-if __name__ == '__main__':
-  
     # ex1
     # x = np.random.random(1024)
     # print(np.allclose(DFT_slow(x), np.fft.fft(x)))
-  # ex2
-    img = io_url.imread('https://img2.zergnet.com/2309662_300.jpg')
+    # ex2
+    img = io_url.imread("https://img2.zergnet.com/2309662_300.jpg")
     gray_img = np.mean(img, -1)
     row_fft, row_col_fft = DFT_2D(gray_img)
     show_img(gray_img, row_fft, row_col_fft)
-
- 
-
-
-
